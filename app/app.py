88b651ae770404d0e3fa11be897563472ee87210
Igor Permyakov <imouttasightx@gmail.com>
2020-06-07 22:00:04 +0600

    Created blog blueprint

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+
 from config import Configuration
+from blog.blueprint import blog
 
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+app.register_blueprint(blog, url_prefix='/blog')
---
+++ app/blog/blueprint.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from flask import render_template
+
+
+blog = Blueprint('blog', __name__, template_folder='templates')
+
+
+@blog.route('/')
+def index():
+    return render_template('blog/index.html')
