f3b58f1f154098e51964a5c82d947c90e86b2fde
Igor Permyakov <imouttasightx@gmail.com>
2020-06-13 21:57:16 +0600

    Created template containing posts for every tag. Now every post shows its tags

--- app/blog/blueprint.py
+++ app/blog/blueprint.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask import render_template
 
 from models import Post
+from models import Tag
 
 
 blog = Blueprint('blog', __name__, template_folder='templates')
@@ -16,4 +17,12 @@ def index():
 @blog.route('/<slug>/')
 def post_details(slug):
     post = Post.query.filter(Post.slug==slug).first()
-    return render_template('blog/post_details.html', post=post)   
+    tags = post.tags
+    return render_template('blog/post_details.html', post=post, tags=tags)
+
+
+@blog.route('/tag/<slug>/')
+def tag_details(slug):
+    tag = Tag.query.filter(Tag.slug==slug).first()
+    posts = tag.posts.all()
+    return render_template('blog/tag_details.html', tag=tag, posts=posts)
