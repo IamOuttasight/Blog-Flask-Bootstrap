bd059308d31f0754f5c474f675e05cfc2ea4b436
Igor Permyakov <imouttasightx@gmail.com>
2020-06-14 22:32:43 +0600

    Added form to create posts

--- app/blog/blueprint.py
+++ app/blog/blueprint.py
@@ -1,12 +1,37 @@
 from flask import Blueprint
 from flask import render_template
 from flask import request
+from flask import redirect
+from flask import url_for
 
 from models import Post
 from models import Tag
+from .forms import PostForm
+from app import db
 
 
 blog = Blueprint('blog', __name__, template_folder='templates')
+
+
+@blog.route('/create/', methods=['POST', 'GET'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        tags = request.form.getlist('tags')
+        try:
+            post = Post(title=title, body=body)
+            for title in tags:
+                tag = Tag.query.filter(Tag.title==title).first()
+                post.tags.append(tag)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('Something went wrong')
+        return redirect(url_for('blog.index'))
+
+    form = PostForm()
+    return render_template('blog/create_post.html', form=form)
 
 
 @blog.route('/')
@@ -17,7 +42,7 @@ def index():
             Post.title.contains(q) | Post.body.contains(q)
             ).all()
     else:
-        posts = Post.query.all()
+        posts = Post.query.order_by(Post.created.desc())
     return render_template('blog/index.html', posts=posts)
 
 
