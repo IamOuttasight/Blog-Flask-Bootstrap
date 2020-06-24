84233684c536ff28ec9a6052e9cbf02ed5b74cb5
Igor Permyakov <imouttasightx@gmail.com>
2020-06-24 22:03:58 +0600

    Added page 404. Close #18

--- app/blog/blueprint.py
+++ app/blog/blueprint.py
@@ -34,7 +34,7 @@ def create_post():
 @blog.route('/<slug>/edit/', methods=['POST', 'GET'])
 @login_required
 def edit_post(slug):
-    post = Post.query.filter(Post.slug==slug).first()
+    post = Post.query.filter(Post.slug==slug).first_or_404()
 
     if request.method == 'POST':
         post.title = request.form['title']
@@ -71,13 +71,13 @@ def index():
 
 @blog.route('/<slug>/')
 def post_details(slug):
-    post = Post.query.filter(Post.slug==slug).first()
+    post = Post.query.filter(Post.slug==slug).first_or_404()
     tags = post.tags
     return render_template('blog/post_details.html', post=post, tags=tags)
 
 
 @blog.route('/tag/<slug>/')
 def tag_details(slug):
-    tag = Tag.query.filter(Tag.slug==slug).first()
+    tag = Tag.query.filter(Tag.slug==slug).first_or_404()
     posts = tag.posts.all()
     return render_template('blog/tag_details.html', tag=tag, posts=posts)
