faa8125eba2798cb8b061c24798d5faa10ee76a1
Igor Permyakov <imouttasightx@gmail.com>
2020-06-24 18:27:48 +0600

    Created User and Role models. Close #16

--- app/models.py
+++ app/models.py
@@ -3,6 +3,10 @@ from time import time
 from datetime import datetime
 from slugify import slugify
 
+from flask_security import UserMixin
+from flask_security import RoleMixin
+import email_validator
+
 
 post_tags = db.Table(
     'post_tags',
@@ -61,3 +65,30 @@ class Tag(db.Model):
 
     def __repr__(self):
         return self.title
+
+
+### Flask Security
+
+users_roles = db.Table(
+    'users_roles',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+    )
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    roles = db.relationship(
+        'Role',
+        secondary=users_roles,
+        backref=db.backref('users', lazy='dynamic')
+        )
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(255))
